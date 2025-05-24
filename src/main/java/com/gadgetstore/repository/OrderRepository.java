5b97c736041c7239c9e7190131423725b06400e1
Claudius26 <omoniyiayomide987@gmail.com>
2025-05-24 02:16:18 +0100

    solid

---
+++ src/main/java/com/gadgetstore/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.gadgetstore.data.repository;
+
+import com.gadgetstore.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserAddress(String userAddress);
+    List<Order> findByStatus(String status);
+}
